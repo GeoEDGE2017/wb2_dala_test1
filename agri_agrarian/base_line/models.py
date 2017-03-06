1001f995dc1e37e528b03fbc4dcbf59dc9d1d16c
Dileepa <dileepa.ozi@mail.com>
2017-03-06 12:48:28 +0530

    fixed the warnings in django server runing

--- agri_agrarian/base_line/models.py
+++ agri_agrarian/base_line/models.py
@@ -18,22 +18,6 @@ class BdSessionKeys(models.Model):
 
 
 # Table 1
-
-class BdSessionKeys(models.Model):
-    data_type = models.CharField(max_length=120, blank=True, null=True)
-    key = models.BigIntegerField(blank=True, null=True)
-    date = models.DateTimeField(blank=True, null=True)
-    user = models.IntegerField(blank=True, null=True)
-    bs_date = models.CharField(max_length=255, blank=True, null=True)
-    table_name = models.CharField(max_length=255, blank=True, null=True)
-    full_bs_date = models.DateField(blank=True, null=True)
-    district = models.ForeignKey(District, db_column='district', related_name='ag_bs_district', blank=True, null=True)
-
-    class Meta:
-        managed = False
-        db_table = 'agri_agrarian\".\"bd_session_keys'
-
-
 class BcagSeasonalCrops(models.Model):
     seasonal_crops = models.CharField(max_length=255, blank=True, null=True)
     areas_cultivated = models.FloatField(blank=True, null=True)

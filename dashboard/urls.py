7ca0d35d3ae6272513ae09ae058f24f3a09baaf4
amithdm <amithmirihella@gmail.com>
2017-03-06 13:42:21 +0530

    Transport Css Fixed

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -11,6 +11,10 @@ urlpatterns = [
 	url(r'^mining_main$', views.mining_main,name='mining_main'),
 	url(r'^othergov_main$', views.othergov_main,name='othergov_main'),
 	url(r'^education_main$', views.education_main,name='education_main'),
+	url(r'^transport_air_main$', views.transport_air_main,name='transport_air_main'),
+	url(r'^transport_land_main$', views.transport_land_main,name='transport_land_main'),
+	url(r'^transport_rail_main$', views.transport_rail_main,name='transport_rail_main'),
+	url(r'^transport_water_main$', views.transport_water_main,name='transport_water_main'),
     url(r'^logout/$',
         'django.contrib.auth.views.logout', {'next_page': '/admin/login/'}, name='logout'
         ),

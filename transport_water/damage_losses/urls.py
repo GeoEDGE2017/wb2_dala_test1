bd058259715fccabea24ba267ebb2b7b00abfe19
Dileepa <dileepa.ozi@mail.com>
2017-03-07 14:35:22 +0530

    fixed merging issues

--- transport_water/damage_losses/urls.py
+++ transport_water/damage_losses/urls.py
@@ -7,8 +7,7 @@ app_name = 'damage_losses'
 urlpatterns = [
 
  url(r'^dl_water_transport$', views.dl_water_transport, name='dl_water_transport'),
- url(r'^dl_summary_trans_water_dis$', views.dl_summary_trans_water_dis, name='dl_summary_trans_water_dis'),
+ url(r'^dl_summary_trans_water_dst$', views.dl_summary_trans_water_dst, name='dl_summary_trans_water_dst'),
  url(r'^dl_summary_trans_water_pro$', views.dl_summary_trans_water_pro, name='dl_summary_trans_water_pro'),
  url(r'^dl_summary_trans_water_nat$', views.dl_summary_trans_water_nat, name='dl_summary_trans_water_nat'),
-
 ]

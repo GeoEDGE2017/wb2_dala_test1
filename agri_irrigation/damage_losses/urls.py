19c8d1d0bdc7949a1bfc5eaaf2a1bec070cf2d18
sachh93 <sachh93@gmail.com>
2017-03-09 05:23:00 +0530

    irrigation damage losses

--- agri_irrigation/damage_losses/urls.py
+++ agri_irrigation/damage_losses/urls.py
@@ -4,4 +4,5 @@ from .import views
 urlpatterns = [
     url(r'^dl_irrigation', views.dl_irrigation, name='dl_irrigation'),
     url(r'^dl_sum_irg_district', views.dl_sum_irg_district, name='dl_sum_irg_district'),
+    url(r'^dl_sum_irg_province', views.dl_sum_irg_province, name='dl_sum_irg_province'),
 ]
---
+++ agri_irrigation/static/agri_irrigation/damage_losses/js/dl_sum_irg_province.js
@@ -0,0 +1,236 @@
+var app = angular.module('dlAgriIrrifationProApp', ['underscore']);
+
+app.controller("DlAgriIrrifationProController", function ($scope, $http, $parse, _) {
+    $scope.district;
+    $scope.incident;
+    $scope.bs_data={};
+    $scope.province = "";
+    $scope.is_edit = false;
+    $scope.submitted = false;
+    $scope.is_valid_data = true;
+    $scope.totaldpub = null;
+    $scope.totaldpvt = null;
+    $scope.totalyear1pub = null;
+    $scope.totalyear1pvt = null;
+    $scope.totalyear2pub = null;
+    $scope.totalyear2vt = null;
+    $scope.finaltotalpublic = null;
+    $scope.finaltotalprivate = null;
+    // declaring total variables
+    $scope.total_num_affected = 0;
+    $scope.grndtotaldpub = 0;
+    $scope.grndtotaldpvt = 0;
+    $scope.grndtotalyear1pub = 0;
+    $scope.grndtotalyear1pvt = 0;
+    $scope.grndtotalyear2pub = 0;
+    $scope.grndtotalyear2pvt = 0;
+    $scope.grndfinaltotalpublic = 0;
+    $scope.grndfinaltotalprivate = 0;
+    $scope.summaryDamages = 0;
+    $scope.summaryLossYear1 = 0;
+    $scope.summaryLossYear2 = 0;
+    $scope.summaryTotal = 0;
+
+    // get relevant damage_losses data for calculations
+    $scope.changedValue = function getDlData(selectProvinces) {
+
+        if($scope.incident && selectProvinces) {
+
+         fetchProvinces();
+        }
+
+    }
+    $scope.provinces = [];
+
+    function fetchProvinces()
+    {
+
+          $http({
+            method: "POST",
+            url: '/fetch_incident_provinces',
+            data: angular.toJson({
+                    'incident': $scope.incident
+                   }),
+            }).success(function(data) {
+                $scope.provinces = data;
+                $scope.province = "";
+                console.log(data);
+
+            })
+
+    }
+
+    $scope.fetchDlData = function(form){
+        $scope.is_edit = true;
+        $scope.submitted = true;
+            $http({
+            method: "POST",
+            url: '/dl_fetch_summary_disagtn',
+            data: angular.toJson({
+            'table_name':  ['Table_8','Table_4','Table_4','Table_3'],
+            'sector': ['transport_land','transport_air','transport_water','transport_rail'],
+            'com_data': {
+                    'province': $scope.province,
+                    'incident': $scope.incident,
+                  },
+                   }),
+            }).success(function(data) {
+
+            angular.forEach(data, function(value, key) {
+             if(key == 'transport_land'){
+              console.log(value.Table_8);
+              angular.forEach(value.Table_8, function(value, key) {
+                 console.log('land',key);
+            });
+              }
+            });
+
+            $scope.dlTransSumPro = data;
+
+            })
+    }
+
+//   $scope.convertToInt = function(val1,val2,val3){
+//
+//        var sum = parseInt(val1) + parseInt(val2) + parseInt(val3);
+//        return sum;
+//    }
+//
+//   $scope.convertTotal = function(val1,val2,val3,val4){
+//
+//        var sum = parseInt(val1) + parseInt(val2) + parseInt(val3) + parseInt(val4) ;
+//        return sum;
+//    }
+//
+//   $scope.getTotal = function(key) {
+//
+//        $scope.finaltotalprivate = 0;
+//
+//
+//
+//        var totaldpub =  ($scope.dlTransSumPro.transport_land.Table_8[key].DlGacPubProvince[0] ? ($scope.dlTransSumPro.transport_land.Table_8[key].DlGacPubProvince[0].damages ?
+//                         $scope.dlTransSumPro.transport_land.Table_8[key].DlGacPubProvince[0].damages : 0):0) +
+//                         ($scope.dlTransSumPro.transport_air.Table_4[key].DlAirDmgPubProvince[0] ? ($scope.dlTransSumPro.transport_air.Table_4[key].DlAirDmgPubProvince[0].tot_destroyed_pub ?
+//                         $scope.dlTransSumPro.transport_air.Table_4[key].DlAirDmgPubProvince[0].tot_destroyed_pub : 0) : 0)+
+//                         ($scope.dlTransSumPro.transport_water.Table_4[key].DlWaterDmgPubProvince[0] ? ($scope.dlTransSumPro.transport_water.Table_4[key].DlWaterDmgPubProvince[0].tot_dmg_public ?
+//                         $scope.dlTransSumPro.transport_water.Table_4[key].DlWaterDmgPubProvince[0].tot_dmg_public : 0) : 0) +
+//                         ($scope.dlTransSumPro.transport_rail.Table_3[key].TotDmgProvince[0] ? ($scope.dlTransSumPro.transport_rail.Table_3[key].TotDmgProvince[0].tot_damages ?
+//                         $scope.dlTransSumPro.transport_rail.Table_3[key].TotDmgProvince[0].tot_damages : 0) : 0);
+//
+//        var totaldpubstring = "totaldpub_"+ key;
+//
+//        var model = $parse(totaldpubstring);
+//        model.assign($scope, totaldpub);
+//
+//        $scope.grndtotaldpub = $scope.grndtotaldpub + totaldpub ;
+//
+//
+//
+//
+//
+//         var totaldpvt =$scope.convertToInt(
+//                         ($scope.dlTransSumPro.transport_land.Table_8[key].DlGacPvtProvince[0]?($scope.dlTransSumPro.transport_land.Table_8[key].DlGacPvtProvince[0].tot_damages_pvt ?
+//                         $scope.dlTransSumPro.transport_land.Table_8[key].DlGacPvtProvince[0].tot_damages_pvt : 0 ) : 0),
+//                         ($scope.dlTransSumPro.transport_air.Table_4[key].DlAirDmgPvtProvince[0] ? ($scope.dlTransSumPro.transport_air.Table_4[key].DlAirDmgPvtProvince[0].tot_destroyed_pvt ?
+//                         $scope.dlTransSumPro.transport_air.Table_4[key].DlAirDmgPvtProvince[0].tot_destroyed_pvt : 0) : 0),
+//                          ($scope.dlTransSumPro.transport_water.Table_4[key].DlWaterDmgPvtProvince[0]?($scope.dlTransSumPro.transport_water.Table_4[key].DlWaterDmgPvtProvince[0].tot_dmg_private ?
+//                         $scope.dlTransSumPro.transport_water.Table_4[key].DlWaterDmgPvtProvince[0].tot_dmg_private : 0
+//                         ):0));
+//
+//        var totaldpvtstring = "totaldpvt_"+ key;
+//
+//        var model = $parse(totaldpvtstring);
+//        model.assign($scope, totaldpvt);
+//        $scope.grndtotaldpvt = $scope.grndtotaldpvt + totaldpvt ;
+//        $scope.summaryDamages = $scope.grndtotaldpub + $scope.grndtotaldpvt;
+//
+//
+//         var totalyear1pub =
+//                         ($scope.dlTransSumPro.transport_land.Table_8[key].DlYearsPubProvince[0]?($scope.dlTransSumPro.transport_land.Table_8[key].DlYearsPubProvince[0].year_1 ?
+//                         $scope.dlTransSumPro.transport_land.Table_8[key].DlYearsPubProvince[0].year_1 : 0):0)  +
+//                         ($scope.dlTransSumPro.transport_air.Table_4[key].DlAirLosProvince[0]?($scope.dlTransSumPro.transport_air.Table_4[key].DlAirLosProvince[0].year_1_pub ?
+//                         $scope.dlTransSumPro.transport_air.Table_4[key].DlAirLosProvince[0].year_1_pub : 0) : 0) +
+//                         ($scope.dlTransSumPro.transport_water.Table_4[key].DlWaterLosProvince[0] ?($scope.dlTransSumPro.transport_water.Table_4[key].DlWaterLosProvince[0].year_1_pub ?
+//                         $scope.dlTransSumPro.transport_water.Table_4[key].DlWaterLosProvince[0].year_1_pub : 0 ) : 0);
+//
+//        var totalyear1pubstring = "totalyear1pub_"+ key;
+//
+//        var model = $parse(totalyear1pubstring);
+//        model.assign($scope, totalyear1pub);
+//        $scope.grndtotalyear1pub = $scope.grndtotalyear1pub + totalyear1pub ;
+//
+//
+//         var totalyear1pvt =
+//                         ($scope.dlTransSumPro.transport_land.Table_8[key].DlOtherLosPvtDistrict[0] ? ($scope.dlTransSumPro.transport_land.Table_8[key].DlOtherLosPvtDistrict[0].year_1_pvt ?
+//                         $scope.dlTransSumPro.transport_land.Table_8[key].DlOtherLosPvtDistrict[0].year_1_pvt : 0) :0) +
+//                         ($scope.dlTransSumPro.transport_air.Table_4[key].DlAirLosProvince[0] ? ($scope.dlTransSumPro.transport_air.Table_4[key].DlAirLosProvince[0].year_1_pvt ?
+//                         $scope.dlTransSumPro.transport_air.Table_4[key].DlAirLosProvince[0].year_1_pvt : 0): 0 )+
+//                         ($scope.dlTransSumPro.transport_water.Table_4[key].DlWaterLosProvince[0] ? ($scope.dlTransSumPro.transport_water.Table_4[key].DlWaterLosProvince[0].year_1_pvt ?
+//                         $scope.dlTransSumPro.transport_water.Table_4[key].DlWaterLosProvince[0].year_1_pvt : 0 ) : 0);
+//
+//        var totalyear1pvtstring = "totalyear1pvt_"+ key;
+//
+//        var model = $parse(totalyear1pvtstring);
+//        model.assign($scope, totalyear1pvt);
+//        $scope.grndtotalyear1pvt = $scope.grndtotalyear1pvt + totalyear1pvt ;
+//        $scope.summaryLossYear1 = $scope.grndtotalyear1pub + $scope.grndtotalyear1pvt;
+//
+//
+//
+//         var totalyear2pub =
+//                         ($scope.dlTransSumPro.transport_land.Table_8[key].DlYearsPubProvince[0]?($scope.dlTransSumPro.transport_land.Table_8[key].DlYearsPubProvince[0].year_2 ?
+//                         $scope.dlTransSumPro.transport_land.Table_8[key].DlYearsPubProvince[0].year_2 : 0 ) :0)+
+//                         ($scope.dlTransSumPro.transport_air.Table_4[key].DlAirLosProvince[0]?($scope.dlTransSumPro.transport_air.Table_4[key].DlAirLosProvince[0].year_2_pub ?
+//                         $scope.dlTransSumPro.transport_air.Table_4[key].DlAirLosProvince[0].year_2_pub : 0) :0)+
+//                         ($scope.dlTransSumPro.transport_water.Table_4[key].DlWaterLosProvince[0]?($scope.dlTransSumPro.transport_water.Table_4[key].DlWaterLosProvince[0].year_2_pub ?
+//                         $scope.dlTransSumPro.transport_water.Table_4[key].DlWaterLosProvince[0].year_2_pub : 0):0);
+//
+//         var totalyear2pubstring = "totalyear2pub_"+ key;
+//
+//         var model = $parse(totalyear2pubstring);
+//         model.assign($scope, totalyear2pub);
+//         $scope.grndtotalyear2pub = $scope.grndtotalyear2pub + totalyear2pub ;
+//
+//         var totalyear2pvt =
+//                         ($scope.dlTransSumPro.transport_land.Table_8[key].DlOtherLosPvtDistrict[0] ?($scope.dlTransSumPro.transport_land.Table_8[key].DlOtherLosPvtDistrict[0].year_2_pub ?
+//                         $scope.dlTransSumPro.transport_land.Table_8[key].DlOtherLosPvtDistrict[0].year_2_pub : 0 ) : 0)+
+//                         ($scope.dlTransSumPro.transport_air.Table_4[key].DlAirLosProvince[0] ?($scope.dlTransSumPro.transport_air.Table_4[key].DlAirLosProvince[0].year_2_pvt ?
+//                         $scope.dlTransSumPro.transport_air.Table_4[key].DlAirLosProvince[0].year_2_pvt : 0) : 0 ) +
+//                         ($scope.dlTransSumPro.transport_water.Table_4[key].DlWaterLosProvince[0] ? ($scope.dlTransSumPro.transport_water.Table_4[key].DlWaterLosProvince[0].year_2_pvt ?
+//                         $scope.dlTransSumPro.transport_water.Table_4[key].DlWaterLosProvince[0].year_2_pvt : 0) : 0) ;
+//
+//         var totalyear2pvtstring = "totalyear2pvt_"+ key;
+//
+//         var model = $parse(totalyear2pvtstring);
+//         model.assign($scope, totalyear2pvt);
+//         $scope.grndtotalyear2pvt = $scope.grndtotalyear2pvt + totalyear2pvt ;
+//         $scope.summaryLossYear2 = $scope.grndtotalyear2pub + $scope.grndtotalyear2pvt;
+//
+//
+//         var finaltotalpublic = totaldpub + totalyear1pub + totalyear2pub;
+//
+//         var finaltotalpublicstring = "finaltotalpublic_"+ key;
+//
+//         var model = $parse(finaltotalpublicstring);
+//         model.assign($scope, finaltotalpublic);
+//         $scope.grndfinaltotalpublic = $scope.grndfinaltotalpublic + finaltotalpublic ;
+//
+//
+//         var finaltotalprivate = totaldpvt + totalyear1pvt + totalyear2pvt;
+//
+//         var finaltotalprivatestring = "finaltotalprivate_"+ key;
+//
+//         var model = $parse(finaltotalprivatestring);
+//         model.assign($scope, finaltotalprivate);
+//         $scope.grndfinaltotalprivate = $scope.grndfinaltotalprivate + finaltotalprivate ;
+//         $scope.summaryTotal = $scope.grndfinaltotalpublic + $scope.grndfinaltotalprivate;
+//
+//
+//
+//    }
+
+
+
+
+
+ })

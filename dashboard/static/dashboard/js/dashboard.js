0239e8e799639214e61a8bd0377ddbaac92cb864
sachh93 <sachh93@gmail.com>
2017-04-26 10:39:07 +0530

    Get Latest Update from Git

--- dashboard/static/dashboard/js/dashboard.js
+++ dashboard/static/dashboard/js/dashboard.js
@@ -8,9 +8,13 @@ $scope.month_losses  = 0;
             method: "POST",
             url: '/fetch_dashboard_data',
         }).success(function(data) {
+
+            $scope.lastIncident = data[0].name;
+            $scope.lastIncidentDamge = data[0].tot_dmgloss;
+            console.log($scope.lastIncident);
             angular.forEach(data,function(value,index){
             $scope.getDate = value.reported_date_time;
-            console.log(value.reported_date_time);
+//            console.log(value.reported_date_time);
             var resMonth = value.reported_date_time.slice(0, 2);
             var resYear = value.reported_date_time.slice(6, 10);
             var d = new Date();

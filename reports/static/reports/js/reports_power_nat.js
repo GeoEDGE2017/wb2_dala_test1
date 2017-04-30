b8993a1035b7430bd20109d8785b12c08854ff38
cham123 <chamupathi2008@gmail.com>
2017-04-30 13:45:31 +0530

    report added to power supply

---
+++ reports/static/reports/js/reports_power_nat.js
@@ -0,0 +1,62 @@
+//Table 6
+var app = angular.module('reportPowerSupplyNatApp', ['underscore']);
+
+app.controller("reportPowerSupplyNatController", function ($scope,$http,$parse, _) {
+    $scope.district;
+    $scope.incident;
+    $scope.bs_data={};
+    $scope.province = "";
+    $scope.is_edit = false;
+    $scope.submitted = false;
+    $scope.is_valid_data = true;
+    $scope.totaldpub = null;
+    $scope.totaldpvt = null;
+    $scope.totalyear1pub = null;
+    $scope.totalyear1pvt = null;
+    $scope.totalyear2pub = null;
+    $scope.totalyear2pvt = null;
+    $scope.finaltotalpublic = null;
+    $scope.finaltotalprivate = null;
+    // declaring total variables
+    $scope.total_num_affected = 0;
+
+     $scope.loadData = function(form){
+        $scope.fetchDlData();
+    }
+
+
+    $scope.fetchDlData = function(){
+
+        $scope.is_edit = true;
+        $scope.submitted = true;
+
+            $http({
+            method: "POST",
+            url: '/dl_fetch_district_disagtn',
+            data: angular.toJson({
+            'table_name':'Table_6',
+            'sector': 'power_supply',
+            'com_data': {
+                    'incident': $scope.incident,
+                  },
+                   }),
+            }).success(function(data) {
+
+            console.log('load ', data);
+            $scope.data= data;
+            $scope.dlPowerSupplySumNat = data;
+
+            })
+
+    }
+            $scope.checkIfNull = function()
+   {
+        var isNull = $scope.dlPowerSupplySumNat ? angular.equals({}, $scope.dlPowerSupplySumNat.power_supply.Table_6) : true;
+        return isNull;
+
+   }
+
+
+
+
+ })

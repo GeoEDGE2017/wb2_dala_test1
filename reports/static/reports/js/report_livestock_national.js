2f4850327bcd20bd59220aae4acf2267907e2429
cham123 <chamupathi2008@gmail.com>
2017-05-01 19:24:25 +0530

    aggri live stock report

---
+++ reports/static/reports/js/report_livestock_national.js
@@ -0,0 +1,122 @@
+var app = angular.module('reportAgriLivestocknNatApp', []);
+
+app.controller("reportAgriLivestocknNatController", ['$scope','$http',function ($scope,$http) {
+    $scope.district;
+    $scope.incident;
+    $scope.bs_data={};
+    $scope.province = "";
+    $scope.is_edit = false;
+    $scope.submitted = false;
+    $scope.is_valid_data = true;
+    $scope.totaldpub = null;
+    $scope.totaldpvt = null;
+    $scope.totalyear1pub = null;
+    $scope.totalyear1pvt = null;
+    $scope.totalyear2pub = null;
+    $scope.totalyear2pvt = null;
+    $scope.finaltotalpublic = null;
+    $scope.finaltotalprivate = null;
+    // declaring total variables
+    $scope.total_num_affected = 0;
+
+    $scope.loadData = function(form){
+        $scope.fetchDlData(form);
+    }
+
+//    $scope.fetchDlData = function(){
+//        $scope.is_edit = true;
+//        $scope.submitted = true;
+//        console.log($scope.incident);
+//            $http({
+//            method: "POST",
+//            url: '/dl_fetch_summary_disagtn',
+//            data: angular.toJson({
+//            'table_name':'Table_6',
+//            'sector': 'agri_livestock',
+//            'com_data': {
+//                    'incident': $scope.incident,
+//                  },
+//                   }),
+//            }).success(function(data) {
+//
+//            console.log('load ', data);
+//            $scope.dlAgriLivestockSumNat = data;
+//
+//            })
+//
+//    }
+
+$scope.fetchDlData = function(form){
+        $scope.is_edit = true;
+        $scope.submitted = true;
+
+            $http({
+            method: "POST",
+            url: '/dl_fetch_summary_disagtn',
+            data: angular.toJson({
+            'table_name':  ['Table_6'],
+            'sector': ['agri_livestock'],
+            'com_data': {
+                    'incident': $scope.incident,
+                  },
+                   }),
+            }).success(function(data) {
+
+            angular.forEach(data, function(value, key) {
+
+            });
+           $scope.dlAgriLivestockSumNat = data  ;
+
+            })
+
+    }
+
+               $scope.checkIfNull = function()
+   {
+        var isNull = $scope.dlAgriLivestockSumNat ? angular.equals({},
+         $scope.dlAgriLivestockSumNat.agri_livestock.Table_6) : true;
+        return isNull;
+
+   }
+
+ $scope.getTotal = function($index,key) {
+         $scope.totaldpub = $scope.totaldpub + ($scope.dlAgriLivestockSumNat.agri_livestock.Table_6[key].DlpNdaPubNational[$index] ? (
+                         $scope.dlAgriLivestockSumNat.agri_livestock.Table_6[key].DlpNdaPubNational[$index].damages ?
+                         $scope.dlAgriLivestockSumNat.agri_livestock.Table_6[key].DlpNdaPubNational[$index].damages : 0 ): 0) ;
+
+         $scope.totaldpvt = $scope.totaldpvt + ($scope.dlAgriLivestockSumNat.agri_livestock.Table_6[key].DlpNdaPvtNational[$index] ? (
+                         $scope.dlAgriLivestockSumNat.agri_livestock.Table_6[key].DlpNdaPvtNational[$index].damages ?
+                         $scope.dlAgriLivestockSumNat.agri_livestock.Table_6[key].DlpNdaPvtNational[$index].damages : 0 ) : 0) ;
+
+         $scope.totalyear1pub = $scope.totalyear1pub + ($scope.dlAgriLivestockSumNat.agri_livestock.Table_6[key].DlpLosPubNational[$index] ? (
+                         $scope.dlAgriLivestockSumNat.agri_livestock.Table_6[key].DlpLosPubNational[$index].los_year_1 ?
+                         $scope.dlAgriLivestockSumNat.agri_livestock.Table_6[key].DlpLosPubNational[$index].los_year_1 : 0 ) : 0) ;
+
+         $scope.totalyear1pvt = $scope.totalyear1pvt + ($scope.dlAgriLivestockSumNat.agri_livestock.Table_6[key].DlpLosPvtNational[$index] ? (
+                         $scope.dlAgriLivestockSumNat.agri_livestock.Table_6[key].DlpLosPvtNational[$index].dmg_los_pvt ?
+                         $scope.dlAgriLivestockSumNat.agri_livestock.Table_6[key].DlpLosPvtNational[$index].dmg_los_pvt : 0 ) : 0 );
+
+         $scope.totalyear2pub = $scope.totalyear2pub + ($scope.dlAgriLivestockSumNat.agri_livestock.Table_6[key].DlpLosPubNational[$index] ? (
+                         $scope.dlAgriLivestockSumNat.agri_livestock.Table_6[key].DlpLosPubNational[$index].los_year_2 ?
+                         $scope.dlAgriLivestockSumNat.agri_livestock.Table_6[key].DlpLosPubNational[$index].los_year_2 : 0 ) : 0) ;
+
+         $scope.totalyear2pvt =($scope.dlAgriLivestockSumNat.agri_livestock.Table_6[key].DlpLosPvtNational[$index] ? (
+                         $scope.dlAgriLivestockSumNat.agri_livestock.Table_6[key].DlpLosPvtNational[$index].los_year_2 ?
+                         $scope.dlAgriLivestockSumNat.agri_livestock.Table_6[key].DlpLosPvtNational[$index].los_year_2 : 0 ) : 0);
+
+
+         $scope.finaltotalpublic = $scope.finaltotalpublic + $scope.totaldpub + $scope.totalyear1pub  + $scope.totalyear2pub;
+
+         $scope.finaltotalprivate = $scope.finaltotalprivate + $scope.totaldpvt + $scope.totalyear1pvt + $scope.totalyear2pvt;
+
+
+
+
+
+
+
+    }
+
+
+
+ }])

10416d8d3d24a57487695b6114f14bb544089a1e
sachh93 <sachh93@gmail.com>
2017-07-29 07:18:36 +0530

    calculation modifications new

--- dala/settings.py
+++ dala/settings.py
@@ -161,7 +161,7 @@ DATABASES = {
         # 'OPTIONS': {
         #     'options': '-c search_path=industry_services'
         # },
-        'NAME': 'dala_2017_07_13',
+        'NAME': 'final_dala_2017',
         'USER': 'postgres',
     },
 }

0d59a8ddad9e9b56d3a23bac031e8d560b841321
Dileepa <dileepa.ozi@mail.com>
2017-08-11 16:50:22 +0530

    latest update of dala

--- dala/settings.py
+++ dala/settings.py
@@ -162,7 +162,8 @@ DATABASES = {
         # },
 
         # 'NAME': 'dala_2017_08_01',
-        'NAME': 'dala_08_09',
+        'NAME': 'test_dala',
+        # 'NAME': 'dala_08_09',
         'USER': 'postgres',
     },
 }

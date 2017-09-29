b982db7c73afb2760d357690cd2ec8d695156f08
sachh93 <sachh93@gmail.com>
2017-09-29 08:47:54 +0530

    modifications alignmnt

--- dala/settings.py
+++ dala/settings.py
@@ -161,7 +161,7 @@ DATABASES = {
         #     'options': '-c search_path=industry_services'
         # },
 
-        'NAME': 'dala_09_15',
+        'NAME': '2017_09_27_DB',
         'USER': 'postgres',
     },
 }

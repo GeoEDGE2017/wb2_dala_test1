f7c9bc4e3be296325334a826025b989d29fdff72
Dileepa <dileepa.ozi@mail.com>
2017-10-17 10:40:34 +0530

    transport land table 8 duplicatate total issue fixed

--- dala/settings.py
+++ dala/settings.py
@@ -161,7 +161,7 @@ DATABASES = {
         #     'options': '-c search_path=industry_services'
         # },
 
-        'NAME': 'dala_10_16',
+        'NAME': 'dala_10_17',
         'USER': 'postgres',
     },
 }

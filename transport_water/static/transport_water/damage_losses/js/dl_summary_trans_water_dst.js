8c4885b1e529bb59096c34ac87515e6bd280d060
Dileepa <dileepa.ozi@mail.com>
2017-10-18 15:40:39 +0530

    transport water table 3 ordering issue fixed 2

--- transport_water/static/transport_water/damage_losses/js/dl_summary_trans_water_dst.js
+++ transport_water/static/transport_water/damage_losses/js/dl_summary_trans_water_dst.js
@@ -57,4 +57,55 @@ bsHealthStatusApp.controller('dlSumTransWaterDstController', function DlEduDistr
             }
         }
     }
+
+    $scope.sumFunc2 = function(val1=0, val2=0) {
+        if(val1 == null) {
+            val1=0;
+        }
+        if(val2 == null) {
+            val2=0;
+        }
+
+        return parseInt(val1) + parseInt(val2);
+    }
+
+    $scope.calGrandTotalPub = function() {
+        console.log('calGrandTotalPub');
+        var tot_los_pub = 0;
+        if(!angular.isUndefined($scope.data)) {
+            angular.forEach($scope.data.transport_water.Table_3, function(value, key, index) {
+                console.log(key);
+                if(key == 'DlWaterLosOther') {
+                    angular.forEach(value, function(value_in, key_in) {
+                        if(value_in.type_los == 'TOTAL LOSSES') {
+                            console.log(value_in);
+                            tot_los_pub = tot_los_pub + $scope.sumFunc2(value_in.tot_los_pub, 0);
+                        }
+                    })
+                }
+            })
+        }
+
+		return tot_los_pub;
+	}
+
+	$scope.calGrandTotalPvt = function() {
+        console.log('calGrandTotalPvt');
+        var tot_los_pvt = 0;
+        if(!angular.isUndefined($scope.data)) {
+            angular.forEach($scope.data.transport_water.Table_3, function(value, key, index) {
+                console.log(key);
+                if(key == 'DlWaterLosOther') {
+                    angular.forEach(value, function(value_in, key_in) {
+                        if(value_in.type_los == 'TOTAL LOSSES') {
+                            console.log(value_in);
+                            tot_los_pvt = tot_los_pvt + $scope.sumFunc2(value_in.tot_los_pvt, 0);
+                        }
+                    })
+                }
+            })
+        }
+
+		return tot_los_pvt;
+	}
 })

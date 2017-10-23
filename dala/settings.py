4ac7b28f92fadd2a1051ceccabdce9589b651b28
amithdm <amithmirihella@gmail.com>
2017-10-23 10:41:41 +0530

    css modification in transport sector

--- dala/settings.py
+++ dala/settings.py
@@ -161,7 +161,7 @@ DATABASES = {
         #     'options': '-c search_path=industry_services'
         # },
 
-        'NAME': 'dala_10_17',
+        'NAME': 'dala_10_09',
         'USER': 'postgres',
     },
 }

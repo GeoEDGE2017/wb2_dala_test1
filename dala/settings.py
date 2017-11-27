55295d90b85235d53d84854ca1ec077e3882ab36
Dileepa <dileepa.ozi@mail.com>
2017-11-27 18:07:55 +0530

    tourism table 5 issue fixed 2

--- dala/settings.py
+++ dala/settings.py
@@ -3174,13 +3174,6 @@ TABLE_PROPERTY_MAPPER = {
             'DlInfLosses': ['avg_val_income_year', 'los_type', 'tol_losses', 'val_income_year1', 'val_income_year2',
                             'val_los_year1', 'val_los_year2', 'id']
         },
-        # 'Table_4': {
-        #
-        #     'DlBusLosses': ['los_type', 'avg_val_income_year', 'val_income_year1', 'val_income_year2', 'val_los_year1',
-        #                     'val_los_year2', 'tol_losses', 'id'],
-        #     'DlInfLosses': ['los_type', 'val_los_year1', 'val_los_year2', 'tol_losses', 'id'],
-        #
-        # },
         'Table_4': {
             'DlDmgBusDistrictN': ['business', 'sumpub', 'sumpvt'],
             'DlLosBusDistrictN': ['business', 'tot_year1_pub', 'tot_year1_pvt', 'tot_year2_pub', 'tot_year2_pvt'],
@@ -3188,10 +3181,15 @@ TABLE_PROPERTY_MAPPER = {
             'DlLosInfDistrictN': ['infrastructure', 'year1', 'year2', 'ownership', 'inf_type']
         },
         'Table_5': {
-            'DlLosInfTotDistrict': ['tot_year1', 'tot_year2', 'ownership'],
-            'DlDmgbusTotDistrict': ['tot_damages', 'ownership'],
-            'DlLosbusTotDistrict': ['los_year1', 'los_year2', 'ownership'],
-            'DlDmgInfTotDistrict': ['sum', 'ownership']
+            # 'DlLosInfTotDistrict': ['tot_year1', 'tot_year2', 'ownership'],
+            # 'DlDmgbusTotDistrict': ['tot_damages', 'ownership'],
+            # 'DlLosbusTotDistrict': ['los_year1', 'los_year2', 'ownership'],
+            # 'DlDmgInfTotDistrict': ['sum', 'ownership'],
+
+            'DlDmgFrmTotProvinceN': ['asset', 'tot_damages_pub', 'tot_damages_pvt'],
+            'DlLosFrmTotProvinceN': ['asset', 'los_year1_pub', 'los_year1_pvt', 'los_year2_pub', 'los_year2_pvt'],
+            'DlDmgInfTotProvinceN': ['assets', 'los_pub', 'los_pvt'],
+            'DlLosInfTotProvinceN': ['asset', 'los_year1_pub', 'los_year1_pvt', 'los_year2_pub', 'los_year2_pvt'],
         },
         'Table_6': {
             'DlDmgInfTotNational': ['sum', 'ownership'],

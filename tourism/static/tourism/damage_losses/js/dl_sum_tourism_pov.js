0d8be08fedd5466a36ebcc87d64ba06eebd47b31
Dileepa <dileepa.ozi@mail.com>
2017-12-12 17:03:13 +0530

    tourism table 5 remove old methods

--- tourism/static/tourism/damage_losses/js/dl_sum_tourism_pov.js
+++ tourism/static/tourism/damage_losses/js/dl_sum_tourism_pov.js
@@ -1,6 +1,5 @@
 //Table 5
 var app = angular.module('dlSummTouBusiFaciPovApp', ['underscore'])
-
 app.controller('dlSummTouBusiFaciPovController', function($scope, $http, $parse, _) {
     $scope.incident;
     $scope.province;
@@ -57,77 +56,11 @@ app.controller('dlSummTouBusiFaciPovController', function($scope, $http, $parse,
                 if(!$scope.data_available) {
                     console.log("no data available for your selection");
                 }
-//                $scope.makeTable();
             }).error(function(err) {
                 $scope.dlSummTouBusiFaciPov = null;
                 $scope.districts = null;
             })
         }
-
-        $scope.makeTable = function() {
-            if($scope.data != null) {
-                $scope.table = {};
-                $scope.table.business = {};
-                $scope.table.infrastructures = {};
-                //district vise objects
-                angular.forEach($scope.districts, function(value, key) {
-                    $scope.table.business[value] = {'name':value }
-                    $scope.table.business[value].year1Damage = {};
-                    $scope.table.business[value].year1Loss = {};
-                    $scope.table.business[value].year2Loss = {};
-
-                    $scope.table.infrastructures[value] = {'name':value }
-                    $scope.table.infrastructures[value].year1Damage = {};
-                    $scope.table.infrastructures[value].year1Loss = {};
-                    $scope.table.infrastructures[value].year2Loss = {};
-
-                    angular.forEach($scope.data[$scope.districts].DlDmgbusTotDistrict, function(value2, key) {
-                        $scope.table.business[value].year1Damage[value2.ownership] = value2.tot_damages;
-                    })
-
-                    angular.forEach($scope.data[$scope.districts].DlLosbusTotDistrict, function(value2, key) {
-                        $scope.table.business[value].year1Loss[value2.ownership] = value2.los_year1;
-                        $scope.table.business[value].year2Loss[value2.ownership] = value2.los_year2;
-                    })
-
-                    angular.forEach($scope.data[$scope.districts].DlDmgInfTotDistrict, function(value2, key) {
-                        $scope.table.infrastructures[value].year1Damage[value2.ownership] = value2.sum;
-                    })
-
-                    angular.forEach($scope.data[$scope.districts].DlLosInfTotDistrict, function(value2, key) {
-                        $scope.table.infrastructures[value].year1Loss[value2.ownership] = value2.tot_year1;
-                        $scope.table.infrastructures[value].year1Loss[value2.ownership] = value2.tot_year2;
-                    })
-                })
-                console.log('table', $scope.table);
-            }
-            else {
-                console.log("data null")
-            }
-        }
-    }
-
-    $scope.getSum3 = function(val1, val2, val3){
-        var final_val = 0;
-        if(!isNaN(val1)) final_val += val1;
-        if(!isNaN(val2)) final_val += val2;
-        if(!isNaN(val3)) final_val += val3;
-
-        return final_val;
-    }
-
-    $scope.getGrandTotCol = function(col) {
-        var final_val = 0;
-        angular.forEach($scope.districtTotals, function(value, key) {
-            final_val += $scope.getConvertedVal( value[col] );
-        })
-        return final_val;
-    }
-
-    $scope.getConvertedVal = function(val) {
-        if(!val)    return 0;
-        if(isNaN(val)) return 0;
-        return val;
     }
 
     $scope.grndTotDmgPub = function() {
